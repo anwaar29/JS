67c9f506c4c2ea67a7cf034972f5acad10c2a5be
anwaar29 <97968236+anwaar29@users.noreply.github.com>
2022-03-03 20:39:14 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,53 @@
+// let age=15;
+// switch(true){
+//     case age<18:
+//         console.log("child");
+//         break;
+//     case 18<age<40:
+//         console.log("Adult");
+//         break;
+//         default:
+//             console.log("old");
+//         break;
+// }
+// let day=0;
+// switch(day){
+//     case (day=0):
+//              console.log(" its Sunday");
+//              break;
+//     case (day=1):
+//             console.log(" its Monday");
+//             break;
+//     case (day=2):
+//             console.log(" its Tuesday");
+//             break;
+//     case (day=3):
+//             console.log(" its Wednesday");
+//             break;
+//     case (day=4):
+//              console.log(" its thursday");
+//             break;
+//     case (day=5):
+//             console.log(" its friday");
+//              break;
+//     default:
+//              console.log(" its Saturday");
+//              break;
+// }
+
+// loops for,while,do while
+// 1 for loop:
+ // var a; // we are declaring
+// var a=20; // initializing the value of variable
+//   it takes three  value/parameter
+//   1initialization
+//   2Condition
+//   3 increment/decrement
+ 
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+let a=19;
+for(let i=1;i<=10;i++){
+    console.log(a+"*"+i+"="+a*i);
+}
